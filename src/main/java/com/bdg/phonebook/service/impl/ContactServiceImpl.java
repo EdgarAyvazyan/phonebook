0119128870a875f7e397b0233546b48515ecc58f
User <khachatryanh1985@gmail.com>
2021-02-15 19:56:34 +0400

    connect

--- src/main/java/com/bdg/phonebook/service/impl/ContactServiceImpl.java
+++ src/main/java/com/bdg/phonebook/service/impl/ContactServiceImpl.java
@@ -6,6 +6,7 @@ import com.bdg.phonebook.service.ContactService;
 import java.util.Scanner;
 import java.util.Set;
 
+
 public class ContactServiceImpl implements ContactService {
 
     private final Scanner scanner = new Scanner(System.in);
@@ -30,12 +31,30 @@ public class ContactServiceImpl implements ContactService {
 
     @Override
     public boolean addContact(Set<Contact> contacts) {
-        //TODO
-        return false;
+        System.out.println("Hello! You entered add contact section. Please, enter contact first name");
+        String command = scanner.next();
+        String firstName = command;
+        System.out.println("Please, enter contact last name");
+        command = scanner.next();
+        String lastName = command;
+        System.out.println("Please, enter contact Phone number");
+        command = scanner.next();
+
+        String phoneNumber = command;
+        System.out.println("Please, enter contact email");
+        command = scanner.next();
+        String email = command;
+        System.out.println("Please, enter contact contact type");
+        command = scanner.next();
+        String contactType = command;
+        contacts.add(new Contact(firstName, lastName, phoneNumber, email, contactType));
+
+        return true;
     }
 
     @Override
     public boolean delete(Set<Contact> contacts) {
+//        contacts.remo;
         //TODO
         return false;
     }
@@ -48,12 +67,23 @@ public class ContactServiceImpl implements ContactService {
 
     @Override
     public void getAllContacts(Set<Contact> contacts) {
-        //TODO
+        for (Contact contact : contacts) {
+            System.out.println(contact.toString());
+        }
     }
 
     @Override
     public boolean deleteContactById(Set<Contact> contacts) {
-        //TODO
+        System.out.println("Please, enter ID of the contact that you want to delete.");
+        String command = scanner.next();
+        for (Contact contact : contacts) {
+            if (Integer.parseInt(command) == contact.getId()) {
+                contacts.remove(contact);
+                return true;
+
+            }
+            System.out.println(contact.toString());
+        }
         return false;
     }
 
